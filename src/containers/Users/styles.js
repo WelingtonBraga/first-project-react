a30c522f66e0c1458cf8ec80a97b78a0eb8dab6a
WelingtonBraga <welingtonbraga10@hotmail.com>
2023-05-19 16:25:09 -0300

    first commit

---
+++ src/containers/Users/styles.js
@@ -0,0 +1,109 @@
+import styled from "styled-components";
+import Backgound from "../../assets/background.svg"
+
+export const Container = styled.div`
+    background: url("${Backgound}");
+    background-size:cover;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 40px;
+`;
+
+export const Image = styled.img`
+    margin-top: 30px;
+`;
+
+export const ConteinerItens = styled.div`
+    background: linear-gradient(157.44deg,
+        rgba(255, 255, 255, 0.6) 0.84%, 
+        rgba(255, 255, 255, 0.6) 0.85%, 
+        rgba(255, 255, 255, 0.15) 100%
+    );
+    
+    border-radius: 61px 61px 0px 0px;
+    backdrop-filter: blur(22.5px);
+
+    padding: 50px 36px;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+`;
+
+export const H1 = styled.h1`
+    font-style: normal;
+    font-weight: normal;
+    font-size: 34px;
+    line-height: 40px;
+    text-align: center;
+    color: #FFFFFF;
+    padding-bottom: 30px;
+`;
+
+export const Button = styled.button`
+    width: 342px;
+    height: 70px;
+    margin-top: 120px;
+
+    background: transparent;
+    border-radius: 14px;
+    border: 1px solid #FFFFFF;
+
+    font-style: normal;
+    font-weight: bold;
+    font-size: 17px;
+    line-height: 28px;
+    color: #FFFFFF;
+
+    cursor: pointer;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+
+    
+
+    &:hover{
+        opacity: 0.8;
+    }
+
+    &:active{
+        opacity: 0.5;
+    }
+
+    img{
+        transform: rotate(180deg);
+    }
+`;
+
+export const User = styled.li`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin-top: 20px;
+
+    background: rgba(255, 255, 255, 0.25);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 14px;
+
+    width: 342px;
+    height: 48px;
+
+    border: none;
+    outline: none;
+
+    p{
+        font-style: normal;
+        font-weight: normal;
+        font-size: 20px;
+        line-height: 28px;
+        color: #FFFFFF;
+    }
+
+    button{
+        background: none;
+        border: none;
+        cursor: pointer;
+    }
+`;
---
+++ src/styles/globalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    overflow-y: hidden;
+    overflow-x: hidden;
+    font-family: 'Roboto', sans-serif;
+    }
+`;
